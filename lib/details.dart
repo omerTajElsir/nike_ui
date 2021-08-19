edb61742db867c83a6d5f46f344adbbbb64f26d8
omer <omer@beuniquegroup.com>
2021-08-19 17:46:28 +0400

    hero transition

--- lib/details.dart
+++ lib/details.dart
@@ -1,9 +1,15 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatefulWidget {
-  DetailsPage({Key? key, required this.index}) : super(key: key);
+  DetailsPage(
+      {Key? key, required this.index, required this.color, required this.image})
+      : super(key: key);
 
   final int index;
+  final Color color;
+  final String image;
 
   @override
   _DetailsPageState createState() => _DetailsPageState();
@@ -13,18 +19,57 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
+        body: Stack(
       children: [
-        Hero(
-          tag: 'header${widget.index}',
-          child: Container(
-            width: 200,
-            height: 100,
-            margin: EdgeInsets.only(right: 40),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink,
+        Positioned(
+          top: -30,
+          child: Hero(
+            tag: 'header${widget.index}',
+            child: Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(300),
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(0)),
+                color: widget.color,
+              ),
             ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150,
+              ),
+              Hero(
+                tag: 'image${widget.index}',
+                child: Transform.rotate(
+                  angle: math.pi / 6,
+                  child: Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.centerRight,
+                      child: Image.asset(
+                        widget.image,
+                        fit: BoxFit.contain,
+                      )),
+                ),
+              ),
+              Text(
+                "Nike Air Max",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
           ),
         )
       ],
--- lib/home.dart
+++ lib/home.dart
@@ -27,7 +27,14 @@ class _MyHomePageState extends State<MyHomePage>
     Color(0xFFFF88BE),
     Color(0xFF85D8C5),
     Color(0xFF746BDE),
-    Color(0xFF85D8C5),
+    Color(0xFFEDA76C),
+  ];
+
+  List<String> Images = [
+    "assets/1.png",
+    "assets/2.png",
+    "assets/3.png",
+    "assets/4.png",
   ];
 
   ValueNotifier<double> notifier = ValueNotifier<double>(0);
@@ -64,10 +71,10 @@ class _MyHomePageState extends State<MyHomePage>
 
     controller = AnimationController(vsync: this);
     colorAnimation =
-        Tween<double>(begin: -math.pi / 4, end: math.pi * notifier.value / 4)
+        Tween<double>(begin: math.pi * notifier.value / 4, end: -math.pi / 3.5)
             .animate(controller);
     colorAnimation1 =
-        Tween<double>(begin: math.pi * notifier.value / 4, end: -math.pi / 4)
+        Tween<double>(begin: -math.pi / 3.5, end: math.pi * notifier.value / 4)
             .animate(controller);
 
     controller.addListener(() {
@@ -87,151 +94,170 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Icon(Icons.check),
-          iconTheme: IconThemeData(color: Colors.black),
-          centerTitle: true,
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.card_travel))
-          ],
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Icon(Icons.check),
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.card_travel))],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
         ),
-        body: ListView(
-          children: [
-            Container(
-              height: 70,
-              padding: EdgeInsets.only(left: 30, right: 30),
-              child: ListView.builder(
-                itemCount: headlines.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return index == 0
-                      ? Container(
-                          padding: EdgeInsets.all(15),
-                          child: Column(
-                            children: [
-                              Text(
-                                headlines[index],
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 18),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Container(
-                                width: 40,
-                                height: 3,
-                                color: Colors.black,
-                              )
-                            ],
-                          ),
-                        )
-                      : Container(
-                          padding: EdgeInsets.all(13),
-                          child: Column(
-                            children: [
-                              Text(
-                                headlines[index],
-                                style:
-                                    TextStyle(color: Colors.grey, fontSize: 18),
-                              ),
-                            ],
-                          ),
-                        );
-                },
-              ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 70,
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: ListView.builder(
+              itemCount: headlines.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return index == 0
+                    ? Container(
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          children: [
+                            Text(
+                              headlines[index],
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: 40,
+                              height: 3,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                      )
+                    : Container(
+                        padding: EdgeInsets.all(13),
+                        child: Column(
+                          children: [
+                            Text(
+                              headlines[index],
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      );
+              },
             ),
-            Container(
-              height: 400,
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: headlines.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
+          ),
+          Container(
+            height: 370,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: Duration(seconds: 1),
+                        pageBuilder: (_, __, ___) => DetailsPage(
+                          index: index,
+                          color: colors[index],
+                          image: Images[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
                     margin: EdgeInsets.all(5),
                     child: Stack(
                       children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              PageRouteBuilder(
-                                transitionDuration: Duration(seconds: 2),
-                                pageBuilder: (_, __, ___) => DetailsPage(
-                                  index: index,
-                                ),
-                              ),
-                            );
-                          },
-                          child: Hero(
-                            tag: 'header$index',
-                            child: Container(
-                              width: 200,
-                              margin: EdgeInsets.only(right: 40),
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                                color: Colors.pink,
-                              ),
-                              padding: EdgeInsets.only(left: 10, top: 30),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Nike Air Max",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text("Men's Fashion Shows",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 14)),
-                                  Text("128 \$",
+                        Hero(
+                          tag: 'header$index',
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(right: 30, bottom: 70),
+                            child: Material(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                              color: colors[index],
+                              child: Container(
+                                width: double.infinity,
+                                padding: EdgeInsets.only(left: 10, top: 30),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Nike Air Max",
                                       style: TextStyle(
                                           color: Colors.white,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold)),
-                                ],
+                                          fontSize: 32,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text("Men's Fashion Shows",
+                                        style: TextStyle(
+                                            color: Colors.white70,
+                                            fontSize: 14)),
+                                    SizedBox(
+                                      height: 25,
+                                    ),
+                                    Text("128 \$",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 28,
+                                            fontWeight: FontWeight.bold)),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                         ),
                         Align(
-                          alignment: Alignment.centerRight,
+                          alignment: Alignment.bottomRight,
                           child: AnimatedBuilder(
                             animation: notifier,
                             builder: (context, _) {
-                              return Transform.rotate(
-                                angle: _currrentPage == index
-                                    ? colorAnimation1.value
-                                    : colorAnimation.value,
-                                child: Container(
-                                    width: 250,
-                                    height: 250,
-                                    alignment: Alignment.centerRight,
-                                    child: Image.asset(
-                                      "assets/1.png",
-                                      fit: BoxFit.contain,
-                                    )),
+                              return Hero(
+                                tag: 'image$index',
+                                child: Transform.rotate(
+                                  angle: _currrentPage == index
+                                      ? colorAnimation.value
+                                      : colorAnimation1.value,
+                                  child: Container(
+                                      margin: EdgeInsets.only(bottom: 20),
+                                      width: MediaQuery.of(context).size.width /
+                                          3 *
+                                          2,
+                                      height:
+                                          MediaQuery.of(context).size.width /
+                                              3 *
+                                              2,
+                                      alignment: Alignment.centerRight,
+                                      child: Image.asset(
+                                        Images[index],
+                                        fit: BoxFit.contain,
+                                      )),
+                                ),
                               );
                             },
                           ),
                         )
                       ],
                     ),
-                  );
-                },
-              ),
-            )
-          ],
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
